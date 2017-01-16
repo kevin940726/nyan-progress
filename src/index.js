63fbcf2a8234e03fc84881f06a7776b35c900167
kevin940726 <kevin830726@gmail.com>
2017-01-17 02:10:01 +0800

    basic implementation, plus rainbow color

--- src/index.js
+++ src/index.js
@@ -1,107 +1,157 @@
 import readline from 'readline';
 
-const clc = require('cli-color');
+const colors = [
+  { name: 'red', code: 9 },
+  { name: 'orange', code: 208 },
+  { name: 'yellow', code: 11 },
+  { name: 'green', code: 10 },
+  { name: 'blue', code: 12 },
+  { name: 'indigo', code: 54 },
+  { name: 'violet', code: 93 },
+];
 
-const NyanProgress = () => ({
-  rainbows: [
-    ['_', '-', '_', ' ', ''],
-    ['-', '_', '-', ' ', ''],
-  ],
-  nyans: {
-    frames: [
-      `_,------,
--|   /\\_/\\
-~|__( o .o)
-   ""  ""   \n`,
-      `-,------,
-_|   /\\_/\\
-~|__( o .o)
-  ""  ""    \n`,
-    ],
-    complete: `_,------,
--|   /\\_/\\
-~|__( ^ .^)
-   ""  ""   \n`,
-    error: `_,------,
--|   /\\_/\\
-~|__( > .<)
-   ""  ""   \n`,
-  },
-  rl: readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  }),
-
-  curr: 0,
-  total: 10,
-  width: 10,
-  interval: 500,
-  animation: 0,
-  complete: false,
-  isRunning: false,
-
-  init(options = {}) {
-    this.curr = options.curr || 0;
-    this.total = options.total || 10;
-    this.width = options.width || this.total;
-    this.interval = options.interval || 500;
-    this.complete = false;
-    this.isRunning = false;
-    this.flag = 0;
-
-    this.animation = setInterval(this.animate.bind(this), this.interval);
-
-    return this;
-  },
-
-  animate() {
-    if (this.isRunning) {
-      process.stdout.write(clc.move.lines(-4));
-    }
-
-    this.isRunning = true;
-
-    this.flag = Number(!this.flag);
-
-    const percent = this.curr / this.total;
-    const len = Math.floor(percent * this.width);
-
-    let nyan = this.nyans.frames[this.flag].split('\n');
-
-    if (this.curr >= this.total) {
-      this.complete = true;
-      nyan = this.nyans.complete.split('\n');
-    }
-
-    const paint = [].concat(...new Array(Math.ceil(len / 2) + 1).fill(this.rainbows))
-      .slice(this.flag, this.flag + len)
-      .concat([nyan])
-      .reduce((p, cur) => p.map((line, i) => line + cur[i]))
-      .join('\n');
-
-    this.rl.write(paint);
-
-    if (this.complete) {
-      clearInterval(this.animation);
-      this.animation = 0;
-    }
-  },
-
-  tick() {
-    this.curr += 1;
-    this.animate();
-  },
+const C = index => str => `\x1b[38;5;${colors[index].code}m${str}\x1b[0m`;
+colors.forEach((c, i) => {
+  const s = C(i)('-');
+  console.log(s, s.length);
 });
 
-// USAGE:
-// const progress = NyanProgress().init({ width: 50 });
-// const timer = setInterval(() => {
-//   progress.tick();
-//
-//   if (progress.complete) {
-//     clearInterval(timer);
-//
-//   }
-// }, 1000);
+function NyanProgress(callback) {
+  const stream = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const nyan = {
+    frames: [
+      ` ,------,
+ |   /\\_/\\
+~|__( o .o)
+   ""  ""   `,
+      ` ,------,
+ |   /\\_/\\
+~|__( o .o)
+  ""  ""    `,
+    ],
+    complete: ` ,------,
+ |   /\\_/\\
+~|__( ^ .^)
+   ""  ""   `,
+    error: ` ,------,
+ |   /\\_/\\
+~|__( > .<)
+   ""  ""   `,
+  };
+
+  const appendVertical = (linesOfStr, ...appendants) => {
+    const result = linesOfStr.slice();
+
+    for (let line = 0; line < result.length; line += 1) {
+      for (let a = 0; a < appendants.length; a += 1) {
+        result[line] += appendants[a][line];
+      }
+    }
+
+    return result;
+  };
+
+  const applyColors = (rainbow) => {
+    const applyByOrder = (base, char) => {
+      if (char === '-') return C(base)(char);
+      else if (char === '_') return C(base + 1)(char);
+      return char;
+    };
+
+    return rainbow.map((line, r) => (
+      line.split('')
+        .map(char => applyByOrder(r * 2, char))
+        .join('')
+    ));
+  };
+
+  let tick = 0;
+
+  return {
+    animation: 0,
+    isComplete: false,
+    rainbow: '',
+
+    start(options = {}) {
+      this.curr = options.curr || 0;
+      this.total = options.total || 10;
+      this.width = options.width || this.total;
+      this.renderThrottle = options.renderThrottle || 500;
+
+      this.isComplete = false;
+
+      this.rainbow = ['_-', '-_', '_-', '- ']
+        .map(line => line.repeat(Math.ceil((this.width + 1) / 2)).substring(0, this.width + 1));
+
+      readline.cursorTo(stream, 0, 0);
+      readline.clearScreenDown(stream);
+
+      this.animation = setInterval(this.animate.bind(this), this.renderThrottle);
+
+      return this;
+    },
+
+    animate() {
+      if (tick) {
+        readline.cursorTo(stream, 0, 0);
+      }
+
+      const portion = this.curr / this.total;
+      const len = Math.floor(portion * this.width);
+      const flag = tick % nyan.frames.length;
+      const cat = nyan.frames[flag].split('\n');
+      const rainbow = this.rainbow.map(line => line.substring(flag, flag + len));
+      this.paint = appendVertical(applyColors(rainbow), cat);
+
+      stream.write(this.paint.join('\n'));
+
+      tick += 1;
+
+      if (this.curr >= this.total) {
+        this.terminate();
+      }
+    },
+
+    tick() {
+      this.curr += 1;
+
+      return this;
+    },
+
+    terminate() {
+      clearInterval(this.animation);
+      this.animation = 0;
+
+      readline.cursorTo(stream, 0, 0);
+      this.paint = appendVertical(applyColors(this.rainbow), nyan.complete.split('\n'));
+      stream.write(this.paint.join('\n'));
+      stream.write('\n');
+
+      this.isComplete = true;
+
+      if (typeof callback === 'function') {
+        callback.call();
+      }
+
+      stream.close();
+
+      return Promise.resolve();
+    },
+  };
+}
+
+const progress = NyanProgress();
+progress.start({ total: 300, width: 50 });
+const timer = setInterval(() => {
+  progress.tick();
+
+  if (progress.isComplete) {
+    clearInterval(timer);
+  }
+}, 100);
 
 export default NyanProgress;
